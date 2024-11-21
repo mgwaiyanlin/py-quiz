061a141e3b6aa299a1e8bd1c8b0a7c5c97a1d301
mgwaiyanlin <waiyanlinn.netuser@gmail.com>
2024-11-21 16:23:20 +0630

    encrypt-decrypter

--- encrypt-decrypter.py
+++ encrypt-decrypter.py
@@ -1,39 +1,43 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 is_out = "no"
-shift_number = 0
+shift_number = int(input("Firstly, give encryption rate (except 0): "))
+array_size_controller = []
 
 def encrypt():
-    secret_message = input("Type your secret message: ").lower()
-    shift_number = int(input("Give encryption rate (except 0): "))
+    secret_message = input("\nType your secret message: ").lower()
     encrypted_message = ""
 
     for letter in secret_message:
         current_index = alphabet.index(letter)
         shifted_index = current_index + shift_number
+        if shifted_index > 25:
+            shifted_index = shifted_index - 25
         encrypted_message += alphabet[shifted_index]
 
     return encrypted_message
 
-def decrypt():
-    encrypted_input = input("Enter your encrypted message: ").lower()
+def decrypt(shift_number):
+    encrypted_input = input("\nEnter your encrypted message: ").lower()
     decrypted_message = ""
-    print(f"The shift number is {shift_number}")
+    
     for letter in encrypted_input:
         current_index = alphabet.index(letter)
         shifted_index = current_index - shift_number
+        if shifted_index < 0:
+            shifted_index = current_index - shift_number - 1
         decrypted_message += alphabet[shifted_index]
 
     return decrypted_message
 
 while is_out == "no":
-    user_choice = input("Type 'encode' to encrypt or Type 'decode' to decrypt: ").lower()
+    user_choice = input("\n\nType 'encode' to encrypt or Type 'decode' to decrypt: ").lower()
 
     if user_choice == "encode":
         encrypted_message = encrypt()
         print(f"Your secret message is encrypted into: {encrypted_message}")
     elif user_choice == "decode":
-        decrypted_message = decrypt()
+        decrypted_message = decrypt(shift_number)
         print(f"The real message is: {decrypted_message}")
     else:
         print("You typed wrong! Try again...")
@@ -41,6 +45,6 @@ while is_out == "no":
     is_out = input("Do you want to exit from the program (yes/no): ")
 
 
-print("\nYou exited from the program!\nBye bye...")
+print("\n\n**You exited from the program!**\nBye bye...")
 
 
